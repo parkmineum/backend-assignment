0ee7371a0c4bc8d17de150e4d299666b63f1a776
parkmineum <a01087164428@gmail.com>
2026-01-18 18:26:00 +0900

    Setting : Application 및 application.yml 세팅

--- src/main/kotlin/parkmineum/exercise_ai/ExerciseAiApplication.kt
+++ src/main/kotlin/parkmineum/exercise_ai/ExerciseAiApplication.kt
@@ -3,7 +3,9 @@ package parkmineum.exercise_ai
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 class ExerciseAiApplication
