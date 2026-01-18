279f39c507c08de1027abb048affbe57cf4ab0bd
parkmineum <a01087164428@gmail.com>
2026-01-18 16:05:56 +0900

    Initial Setting

---
+++ src/main/kotlin/parkmineum/exercise_ai/ExerciseAiApplication.kt
@@ -0,0 +1,11 @@
+package parkmineum.exercise_ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExerciseAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExerciseAiApplication>(*args)
+}
